4843e6b9146e60a80b64b778f3878d6409301e68
shifaalkasasbeh <100359607+shifaalkasasbeh@users.noreply.github.com>
2022-06-26 23:47:43 +0300

    main

---
+++ masterpice/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('master.home');
+});
+
+
+Route::get('/about', function () {
+    return view('master.about');
+});
+
+Route::get('/shop', function () {
+    return view('master.shop');
+});
+
+Route::get('/contact', function () {
+    return view('master.contact');
+});
+
+Route::get('/cart', function () {
+    return view('master.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('master.checkout');
+});
+
+Route::get('/product', function () {
+    return view('master.product');
+});
+
+Route::get('/post', function () {
+    return view('master.post');
+});
+
+
+
